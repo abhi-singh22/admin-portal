845524b1d78c10c968aa636e833a86c7e19bd423
abhi-singh22 <125460653+abhi-singh22@users.noreply.github.com>
2023-02-16 13:17:14 +0530

    updated for error message

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,40 +4,43 @@ import React, { useState } from 'react';
 import axios from "axios";
 
 function Home() {
-
-    // const initialState = {
-    //     firstName: "",
-    //     lastName: "",
-    //     gender: "",
-    //     dob: "",
-    //     race: "",
-    //     ethenticity: "",
-    //     carrierName: "",
-    //     memberId: "",
-    //     groupName: "",
-    //     relationship: "",
-    // }
+    const initialErrorMessege = {
+        firstName: false,
+        lastName: false,
+        gender: false,
+        dob: false,
+        race: false,
+        carrierName: false,
+        memberId: false,
+    };
 
     const [formValues, setFormValues] = useState({});
     const [isFormValid, setIsFormValid] = useState(true);
     const [isInsuranceValid, setIsInsuranceValid] = useState();
+    const [errorMessege, setErrorMessege] = useState(initialErrorMessege);
 
+    const genderOptions = [{ label: 'Select', value: '' }, { label: 'Male', value: 'male' }, { label: 'Female', value: 'female' }, { label: 'Other', value: 'other' }]
+    const raceOptions = [{ label: 'Select', value: '' }, { label: 'American Indian or Alaskan Native', value: 'americanIndian' }, { label: 'Asian', value: 'asian' }, { label: 'Black or African American', value: 'black' }, { label: 'White', value: 'white' }, { label: 'Decline to Answer', value: 'decline' }];
+    const ethenticityOptions = [{ label: 'Select', value: '' }, { label: 'Hispanic or Latin', value: 'hispanic' }, { label: 'Non-Hispanic or Latin', value: 'nonHispanic' }, { label: 'Decline to Answer', value: 'decline' }];
+    const carrierNameOptions = [{ label: 'Select', value: '' }, { label: 'Aetna', value: 'aetna' }, { label: 'BCBS', value: 'bcbs' }, { label: 'Kaiser', value: 'kaiser' }, { label: 'Optum', value: 'optum' }];
+    const relationshipOptions = [{ label: 'Select', value: '' }, { label: 'Self', value: 'self' }, { label: 'Spouse', value: 'spouse' }, { label: 'Parent', value: 'parent' }, { label: 'Child', value: 'child' }, { label: 'Other', value: 'other' }];
+    const mandatoryFields = ["firstName", "lastName", "gender", "dob", "race", "carrierName", "memberId"]
     const handleSubmit = async (event) => {
         event.preventDefault();
         const options = {
             method: 'POST',
             url: 'https://eligibilitycheck.onrender.com/valid-insurance',
             data: formValues
-          };
-          axios
+        };
+        axios
             .request(options)
             .then(function (response) {
                 setIsInsuranceValid(response.data.message)
             })
             .catch(function (error) {
-              new Error(error)
+                new Error(error)
             });
-        setTimeout(()=>{setIsInsuranceValid()}, 6000)
+        setTimeout(() => { setIsInsuranceValid() }, 6000)
     }
 
     function handleUserInput(e) {
@@ -46,15 +49,12 @@ function Home() {
         setFormValues({ ...formValues, [name]: value.trim() });
     }
 
-    function validateInsurance() {
-
+    function showErrorMessege(e) {
+        const name = e.target.name;
+        const value = e.target.value.trim();
+        setErrorMessege({ ...errorMessege, [name]: !value.length })
+        return
     }
-
-    const genderOptions = [{ label: 'Select', value: '' }, { label: 'Male', value: 'male' }, { label: 'Female', value: 'female' },{ label: 'Other', value: 'other' }]
-    const raceOptions = [{ label: 'Select', value: '' }, { label: 'American Indian or Alaskan Native', value: 'americanIndian' }, { label: 'Asian', value: 'asian' }, { label: 'Black or African American', value: 'black' }, { label: 'White', value: 'white' }, { label: 'Decline to Answer', value: 'decline' }];
-    const ethenticityOptions = [{ label: 'Select', value: '' }, { label: 'Hispanic or Latin', value: 'hispanic' }, { label: 'Non-Hispanic or Latin', value: 'nonHispanic' }, { label: 'Decline to Answer', value: 'decline' }];
-    const carrierNameOptions = [{ label: 'Select', value: '' }, { label: 'Aetna', value: 'aetna' }, { label: 'BCBS', value: 'bcbs' }, { label: 'Kaiser', value: 'kaiser' }, { label: 'Optum', value: 'optum' }];
-    const relationshipOptions = [{ label: 'Select', value: '' }, { label: 'Self', value: 'self' }, { label: 'Spouse', value: 'spouse' },{ label: 'Parent', value: 'parent' },{ label: 'Child', value: 'child' }, { label: 'Other', value: 'other' }];
 
     return (
         <div className="border-box">
@@ -64,28 +64,32 @@ function Home() {
                     <div className="fieldsRows">
                         <span>
                             <label htmlFor="firstName">First Name<span className="req">*</span></label>
-                            <input id="name" type="text" name="firstName" placeholder="First Name" onChange={(event) => handleUserInput(event)}></input>
-                            {!formValues.firstName && <span className="req">First Name is Required</span>}
+                            <input id="name" type="text" name="firstName" placeholder="First Name" onChange={(event) => handleUserInput(event)} onBlur={(event) => showErrorMessege(event)}></input>
+                            {(errorMessege.firstName) && <span className="req">First Name is Required</span>}
                         </span>
                         <span>
                             <label htmlFor="lastName">Last Name<span className="req">*</span></label>
-                            <input id="name" type="text" name="lastName" placeholder="Last Name" onChange={(event) => handleUserInput(event)}></input>
+                            <input id="name" type="text" name="lastName" placeholder="Last Name" onChange={(event) => handleUserInput(event)} onBlur={(event) => showErrorMessege(event)}></input>
+                            {(errorMessege.lastName) && <span className="req">Last Name is Required</span>}
                         </span>
                     </div>
                     <div className="fieldsRows">
                         <span>
                             <label htmlFor="gender">Gender<span className="req">*</span></label>
-                            <Select options={genderOptions} handleInput={handleUserInput} name="gender" id="gender" />
+                            <Select options={genderOptions} handleInput={handleUserInput} name="gender" id="gender" showErrorMessege={showErrorMessege}/>
+                            {(errorMessege.gender) && <span className="req">Gender is Required</span>}
                         </span>
                         <span>
                             <label htmlFor="dob">DOB<span className="req">*</span></label>
-                            <input id="dob" type="date" name="dob" placeholder="MM/DD/YYYY" onChange={(event) => handleUserInput(event)}></input>
+                            <input id="dob" type="date" name="dob" placeholder="MM/DD/YYYY" onChange={(event) => handleUserInput(event)} onBlur={(event) => showErrorMessege(event)}></input>
+                            {(errorMessege.dob) && <span className="req">DOB is Required</span>}
                         </span>
                     </div>
                     <div className="fieldsRows">
                         <span>
                             <label htmlFor="race">Race<span className="req">*</span></label>
-                            <Select options={raceOptions} handleInput={handleUserInput} name="race" id="race" />
+                            <Select options={raceOptions} handleInput={handleUserInput} name="race" id="race" showErrorMessege={showErrorMessege}/>
+                            {(errorMessege.race) && <span className="req">Race is Required</span>}
                         </span>
                         <span>
                             <label htmlFor="ethenticity">Ethenticity</label>
@@ -98,13 +102,15 @@ function Home() {
                     <div className="carrierClass">
                         <span>
                             <label htmlFor="carrierName">Carrier Name<span className="req">*</span></label>
-                            <Select options={carrierNameOptions} handleInput={handleUserInput} name="carrierName" id="carrierName" />
+                            <Select options={carrierNameOptions} handleInput={handleUserInput} name="carrierName" id="carrierName" showErrorMessege={showErrorMessege}/>
+                            {(errorMessege.carrierName) && <span className="req">Carrier Name is Required</span>}
                         </span>
                     </div>
                     <div className="fieldsRows">
                         <span>
                             <label htmlFor="memberId">Member ID<span className="req">*</span></label>
-                            <input id="memberId" type="text" name="memberId" placeholder="Member ID" onChange={(event) => handleUserInput(event)}></input>
+                            <input id="memberId" type="text" name="memberId" placeholder="Member ID" onChange={(event) => handleUserInput(event)} onBlur={(event) => showErrorMessege(event)}></input>
+                            {(errorMessege.memberId) && <span className="req">Member ID is Required</span>}
                         </span>
                         <span>
                             <label htmlFor="groupNumber">Group Number</label>
@@ -118,12 +124,12 @@ function Home() {
                         </span>
                     </div >
                     <div className="message-box">
-                    {(isInsuranceValid == 'valid') && <div className="valid-insurance-message message-box">
-                            <span>'Valid Insurance' <br/> Patient Health Insurance Validated </span>
-                    </div>}
-                    {(isInsuranceValid == 'invalid') && <div className="invalid-insurance-message message-box">
-                            <span>'Invalid Insurance' <br/> Please check details entered & Submit again </span>
-                    </div>}
+                        {(isInsuranceValid == 'valid') && <div className="valid-insurance-message message-box">
+                            <span>'Valid Insurance' <br /> Patient Health Insurance Validated </span>
+                        </div>}
+                        {(isInsuranceValid == 'invalid') && <div className="invalid-insurance-message message-box">
+                            <span>'Invalid Insurance' <br /> Please check details entered & Submit again </span>
+                        </div>}
                     </div>
                     <div>
                         <button type="submit" disabled={!isFormValid} className="validate-buttom"><b>Validate Insurance</b></button>
--- src/components/Select.js
+++ src/components/Select.js
@@ -1,9 +1,9 @@
 import React from "react";
 
 function Select(props) {
-    const { options, handleInput, name, id } = props;
+    const { options, handleInput, name, id, showErrorMessege } = props;
     return (
-        <select name={name} id={id} onChange={(event) => handleInput(event)}>
+        <select name={name} id={id} onChange={(event) => handleInput(event)} onBlur={(event) => showErrorMessege(event)}>
             {options && options.map((option) => (
                 <option value={option.value} key={option.value}>{option.label}</option>
             ))
