db8d3d408efdc6404f1358ee2d67028ff168dcae
abhi-singh22 <125460653+abhi-singh22@users.noreply.github.com>
2023-02-16 18:05:55 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from "react";
 import Home from '../src/components/Home'
+import Header from '../src/components/Header'
 function App() {
   return (
+    <div>
+      <Header/>
       <Home/>
+    </div>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header style={{marginBottom: '20px'}}>
+        <div className="header">
+            <span></span>
+            <span style={{fontSize: 'xx-large',
+    color: '#1f85ec'}}>Admin Portal</span>
+            <span style={{marginTop:"8px"}}>Welcome User</span>
+        </div>
+      <nav>
+        <div className="tab-container">
+          <div style={{marginLeft:'10px',marginRight:'10px'}}><a href="#">Home</a></div>
+          <div><a href="#">Drive up Registration</a></div>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
